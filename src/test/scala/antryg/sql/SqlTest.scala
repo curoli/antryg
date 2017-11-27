3b0093f5093001c732a76b3d3c1d4503d2836fe8
Oliver Ruebenacker <oliverr@broadinstitute.org>
2017-11-27 18:03:00 -0500

    Print descriptions of table columns.

--- src/test/scala/antryg/sql/SqlTest.scala
+++ src/test/scala/antryg/sql/SqlTest.scala
@@ -4,6 +4,7 @@ import java.nio.file.Paths
 
 import antryg.util.conf.PropertiesTree
 import org.scalatest.FunSuite
+import scalikejdbc.interpolation.SQLSyntax
 import scalikejdbc.{ConnectionPool, DB, scalikejdbcSQLInterpolationImplicitDef}
 
 class SqlTest extends FunSuite {
@@ -16,10 +17,20 @@ class SqlTest extends FunSuite {
     val dbCreds = dbsCreds(dbName)
     ConnectionPool.singleton(dbCreds.url, dbCreds.user, dbCreds.password)
     println(dbCreds.url)
-    val dbs = DB.readOnly { implicit session =>
+    val tables = DB.readOnly { implicit session =>
       sql"show tables".map(_.string("Tables_in_dig_dev")).list.apply
     }
-    println(dbs)
+    println(s"${tables.size} tables: ${tables.mkString(", ")}.")
+    val colReports = tables.map { table =>
+      val cols = DB.readOnly { implicit session =>
+        sql"describe ${SQLSyntax.createUnsafely(table)}".map(rs =>
+          s"${rs.string("Field")} (${rs.string("Type")})"
+        ).list.apply
+      }
+      s"$table (${cols.size} cols): ${cols.mkString(", ")}"
+    }
+    val report = s"${tables.size} tables: ${colReports.mkString("\n", "\n", "\n")}."
+    println(report)
   }
 
 }
