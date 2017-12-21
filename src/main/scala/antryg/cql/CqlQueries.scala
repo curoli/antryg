ff7fe5c57dd9c853b95ad7ec18fe9e2c67f93a72
Oliver Ruebenacker <oliverr@broadinstitute.org>
2017-12-21 16:56:13 -0500

    Copy over some rows of Sample table from MySQL to Cassandra.

--- src/main/scala/antryg/cql/CqlQueries.scala
+++ src/main/scala/antryg/cql/CqlQueries.scala
@@ -1,5 +1,6 @@
 package antryg.cql
 
+import com.datastax.driver.core.querybuilder.{Insert, QueryBuilder}
 import com.datastax.driver.core.schemabuilder.{Create, SchemaBuilder}
 
 object CqlQueries {
@@ -16,6 +17,14 @@ object CqlQueries {
       createTableStmt = createTableStmt.addColumn(otherCol.name, otherCol.dataType)
     }
     createTableStmt
+  }
+
+  def insert(keyspace: String, table:String, values: Map[String, Any]): Insert = {
+    var insert = QueryBuilder.insertInto(keyspace, table)
+    for((key, value) <- values) {
+      insert = insert.value(key, value.asInstanceOf[AnyRef])
+    }
+    insert
   }
 
 }
--- src/test/scala/antryg/portal/SamplesTableTest.scala
+++ src/test/scala/antryg/portal/SamplesTableTest.scala
@@ -31,29 +31,21 @@ class SamplesTableTest extends FunSuite {
     println(createKeyspaceStmt.getQueryString())
     val createKeyspaceResult = session.execute(createKeyspaceStmt).one()
     println(createKeyspaceResult)
-    val table = s"table${Random.alphanumeric.take(10).mkString("")}"
     val samplesTableSqlCols = sqlDb.queryReadOnly(SqlQueries.describeTable(PortalDbSchema.samplesTable))
     val samplesSqlSchema = SqlTableSchema(PortalDbSchema.samplesTable, samplesTableSqlCols)
     println(samplesSqlSchema)
-    val sqlTableResult = sqlDb.queryReadOnly(SqlQueries.selectAll(PortalDbSchema.samplesTable, Some(100)))
-    var allValues: Map[String, Set[Any]] = Map.empty
-    for (row <- sqlTableResult) {
-      for((key, value) <- row) {
-        val keyValuesNew = allValues.getOrElse(key, Set.empty) + value
-        allValues += (key -> keyValuesNew)
-      }
-    }
-    println(allValues)
-    println(allValues.mapValues(_.size))
-    for((key, keyValues) <- allValues) {
-      println(s"$key (${keyValues.size}): ${keyValues.mkString(", ")}".take(120))
-    }
     val cqlTableSchema =
       SqlToCql.convertTable(samplesSqlSchema, SqlToCql.TypeConverters.defaultWithTextFallback, Seq("ID"), Seq())
     val createTableStmt = CqlQueries.createTable(keyspace, cqlTableSchema)
     println(createTableStmt)
     val createTableResult = session.execute(createTableStmt)
     println(createTableResult)
+    Thread.sleep(1000)
+    val sqlTableResult = sqlDb.queryReadOnly(SqlQueries.selectAll(PortalDbSchema.samplesTable, Some(100)))
+    for(values <- sqlTableResult) {
+      val insertStmt = CqlQueries.insert(keyspace, PortalDbSchema.samplesTable, values)
+      session.execute(insertStmt)
+    }
     val dropKeyspaceStmt = SchemaBuilder.dropKeyspace(keyspace)
     println(dropKeyspaceStmt.ifExists().getQueryString())
     val dropKeyspaceResult = session.execute(dropKeyspaceStmt)
