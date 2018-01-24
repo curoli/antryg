14778299821ae5e72ac2eaa6ccab379e2e64f166
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-01-24 15:14:02 -0500

    Cleanup: removing unused code.

--- src/main/scala/antryg/portal/sqltocql/VariantFinderLoader.scala
+++ src/main/scala/antryg/portal/sqltocql/VariantFinderLoader.scala
@@ -2,9 +2,9 @@ package antryg.portal.sqltocql
 
 import antryg.portal.cql.VariantFinderFacade
 import antryg.portal.cql.VariantFinderSchema.{Cols => CqlCols}
-import antryg.portal.sql.{PortalSqlQueries, PortalSqlSchema}
+import antryg.portal.sql.PortalSqlQueries
 import antryg.portal.sql.PortalSqlSchema.VariantMainTable.{Cols => SqlCols}
-import antryg.sql.{SqlDb, SqlQueries}
+import antryg.sql.SqlDb
 
 class VariantFinderLoader(sqlDb: SqlDb, variantFinderFacade: VariantFinderFacade, variantIdSampler: VariantIdSampler) {
 
@@ -15,7 +15,6 @@ class VariantFinderLoader(sqlDb: SqlDb, variantFinderFacade: VariantFinderFacade
   ).mapValues(_.name)
 
   def loadVariantMainTable(): Unit = {
-    val sqlColNames = Seq(SqlCols.variantId, SqlCols.chromosome, SqlCols.position)
     val rows = sqlDb.queryReadOnly(PortalSqlQueries.selectVariantCoreData())
     rows.foreach { row =>
       if (variantIdSampler(row.variantId)) {
