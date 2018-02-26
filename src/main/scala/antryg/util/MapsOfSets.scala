482a0ef8273912074ec1bf1d09cd057794423aac
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-02-26 17:09:16 -0500

    MapsOfSets, part 1.

---
+++ src/main/scala/antryg/util/MapsOfSets.scala
@@ -0,0 +1,10 @@
+package antryg.util
+
+object MapsOfSets {
+
+  def fromMaps[K, V](maps: Iterable[Map[K, V]]): Map[K, Set[V]] = {
+    val keys = maps.map(_.keySet).fold(Set.empty)(_ ++ _)
+    keys.map(key => (key, maps.flatMap(_.get(key)).toSet)).toMap
+  }
+
+}
