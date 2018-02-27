6bd4ff9da3a2f7cb4a5c06f991e2650b7812500c
Oliver Ruebenacker <oliverr@broadinstitute.org>
2018-02-27 18:28:56 -0500

    MapOfSets, complete (unions, intersections)

--- src/main/scala/antryg/util/MapOfSets.scala
+++ src/main/scala/antryg/util/MapOfSets.scala
@@ -7,4 +7,19 @@ object MapOfSets {
     keys.map(key => (key, maps.flatMap(_.get(key)).toSet)).toMap
   }
 
+  def filterForKey[K, V](map: Map[K, Set[V]], key: K, filter: V => Boolean): Map[K, Set[V]] = {
+    val setFiltered = map.getOrElse(key, Set.empty).filter(filter)
+    map + (key -> setFiltered)
+  }
+
+  def unions[K, V](map1: Map[K, Set[V]], map2: Map[K, Set[V]]): Map[K, Set[V]] = {
+    val keys = map1.keySet ++ map2.keySet
+    keys.map(key => (key, map1.getOrElse(key, Set.empty) ++ map2.getOrElse(key, Set.empty))).toMap
+  }
+
+  def intersections[K, V](map1: Map[K, Set[V]], map2: Map[K, Set[V]]): Map[K, Set[V]] = {
+    val keys = map1.keySet.intersect(map2.keySet)
+    keys.map(key => (key, map1(key).intersect(map2(key)))).filter(_._2.nonEmpty).toMap
+  }
+
 }
--- src/test/scala/antryg/util/MapOfSetsTest.scala
+++ src/test/scala/antryg/util/MapOfSetsTest.scala
@@ -14,4 +14,27 @@ class MapOfSetsTest extends FunSuite {
     assert(mapOfSets === expectedMapOfSets)
   }
 
+  test("filterForKey") {
+    val map = Map(1 -> Set(1, 2, 3), 2 -> Set(4, 5, 6, 7, 8), 3 -> Set(10, 20, 30))
+    val filteredMap = MapOfSets.filterForKey(map, 2, (num: Int) => num % 2 == 0)
+    val expectedFilteredMap = Map(1 -> Set(1, 2, 3), 2 -> Set(4, 6, 8), 3 -> Set(10, 20, 30))
+    assert(filteredMap === expectedFilteredMap)
+  }
+
+  test("unions") {
+    val map1 = Map(1 -> Set(1, 2, 3), 2 -> Set(4), 3 -> Set(4, 5, 6))
+    val map2 = Map(1 -> Set(4, 5, 6), 2 -> Set(4), 7 -> Set(10, 20, 30))
+    val mapOfUnions = MapOfSets.unions(map1, map2)
+    val expectedMapOfUnions = Map(1 -> Set(1, 2, 3, 4, 5, 6), 2 -> Set(4), 3 -> Set(4, 5, 6), 7 -> Set(10, 20, 30))
+    assert(mapOfUnions === expectedMapOfUnions)
+  }
+
+  test("intersections") {
+    val map1 = Map(1 -> Set(1, 2, 3), 2 -> Set(4, 5), 3 -> Set(4, 5, 6))
+    val map2 = Map(1 -> Set(4, 5, 6), 2 -> Set(5, 6), 3 -> Set(3, 4), 7 -> Set(10, 20, 30))
+    val mapOfUnions = MapOfSets.intersections(map1, map2)
+    val expectedMapOfUnions = Map(2 -> Set(5), 3 -> Set(4))
+    assert(mapOfUnions === expectedMapOfUnions)
+  }
+
 }
